77cbe62e21e7aa92d3d208c77231e6f94e9b1bf5
younger1 <youngger1@helixtech.co.kr>
2023-06-10 18:26:41 +0900

    create project

---
+++ study1/study1/study1App.swift
@@ -0,0 +1,17 @@
+//
+//  study1App.swift
+//  study1
+//
+//  Created by 김상우 on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct study1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
